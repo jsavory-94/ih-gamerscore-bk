ea35eb1838f04453036a55c702acee7ca39340aa
jsavory-94 <james.pa.savory@gmail.com>
2018-03-05 11:17:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const userStats = require('./routes/user-stats');
+const mongoose = require('mongoose');
+const auth = require('./routes/auth');
+const session    = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+mongoose.connect('mongodb://localhost/back');
+
+var app = express();
+
+  app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+  }));
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+app.use(function (req, res, next) {
+  app.locals.user = req.session.currentUser;
+  next();
+});
+
+// view engine setup
+
+// uncomment after placing your favicon in /public
+  
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/user-stats', userStats);
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({error: 'not found'});
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({error: 'unexpected'});
+  }
+});
+
+module.exports = app;
